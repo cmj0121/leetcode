7eee2447e9b77d54569195114617e799a6f697d7
cmj <cmj@cmj.tw>
2021-06-26 11:38:25 +0800

    [0201] Bitwise AND of Numbers Range
    	NOTE
    		may more faster with reduce bit-operartions

---
+++ problems/0201-bitwise-and-of-nubmbers-range/main.rs
@@ -0,0 +1,22 @@
+fn range_bitwise_and(left: i32, right: i32) -> i32 {
+    let mut diff = right - left;
+    let mut ret = left;
+    let mut bits = 0;
+
+    while ret > 0 && diff > 0 {
+        ret = ret >> 1;
+        diff /= 2;
+        bits += 1;
+    }
+
+    ret = ret << bits;
+
+    ret & right
+}
+
+fn main() {
+    assert_eq!( range_bitwise_and(5, 7), 4);
+    assert_eq!( range_bitwise_and(0, 0), 0);
+    assert_eq!( range_bitwise_and(1, 2147483647), 0);
+    assert_eq!( range_bitwise_and(3, 4), 0);
+}
