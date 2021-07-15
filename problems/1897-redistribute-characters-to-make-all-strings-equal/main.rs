0303ebd4d4247d741fed682d358c7221e8841de7
cmj <cmj@cmj.tw>
2021-07-15 08:41:10 +0800

    [1897] Redistribute Characters to Make All Strings Equal

---
+++ problems/1897-redistribute-characters-to-make-all-strings-equal/main.rs
@@ -0,0 +1,41 @@
+
+fn make_equal(words: Vec<String>) -> bool {
+    /*
+        // Slow version
+        let len = words.len();
+        let mut cache: HashMap<char, usize> = HashMap::new();
+
+        for word in words.iter() {
+            for ch in word.chars() {
+                cache.insert( ch, cache.get(&ch).unwrap_or(&0)+1 );
+            }
+        }
+
+        for (_, count) in cache.iter() {
+            if count % len != 0 {
+                return false;
+            }
+        }
+    */
+
+    // still the slow version
+    let len = words.len();
+    let s = words.join("");
+    let mut chars = s.chars().collect::<Vec<char>>();
+
+    chars.sort_by(|x, y| x.cmp(y));
+    for idx in (0..chars.len()).step_by(len) {
+        if idx + len - 1 >= chars.len() {
+            return false;
+        } else if chars[idx] != chars[idx + len - 1] {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn main() {
+    assert_eq!( make_equal( vec!["abc".to_string(),"aabc".to_string(),"bc".to_string()]), true);
+    assert_eq!( make_equal( vec!["ab".to_string(),"b".to_string()]), false);
+}
