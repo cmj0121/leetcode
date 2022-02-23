cab05bacdc0bc8f749ae18cc7f16a91b1bd73d01
cmj <cmj@cmj.tw>
2022-02-23 11:37:09 +0800

    [0338] Counting Bits
    	using the single-loop (two variables) to iterate
    
    	lowest memory usage

---
+++ problems/0338-counting-bits/main.rs
@@ -0,0 +1,28 @@
+fn count_bits(n: i32) -> Vec<i32> {
+    match n {
+        0 => vec![0],
+        1 => vec![0, 1],
+        _ => {
+            let mut ret = vec![0, 1];
+            let mut x = 0;
+            let mut y = ret.len();
+
+            for _m in 1..n {
+                ret.push( ret[x] + 1);
+                x += 1;
+                if x == y {
+                    x = 0;
+                    y *= 2;
+                }
+            }
+
+            ret
+        }
+    }
+}
+
+fn main() {
+    assert_eq!(count_bits(0), vec![0]);
+    assert_eq!(count_bits(2), vec![0, 1, 1]);
+    assert_eq!(count_bits(5), vec![0, 1, 1, 2, 1, 2]);
+}
